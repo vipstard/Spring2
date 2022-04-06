98036510a63e7144d4f54860245fcc1fe8ef89b9
vipstard <vipstar_d@naver.com>
2022-04-06 23:26:29 +0900

    22.04.06

--- core/core/src/main/java/hello/core/member/MemberRepository.java
+++ core/core/src/main/java/hello/core/member/MemberRepository.java
@@ -2,8 +2,8 @@ package hello.core.member;
 
 public interface MemberRepository {
 
-    void save(Member member);
+    void save(Member member); //DB에 저장
 
-    Member findById(Long memberId);
+    Member findById(Long memberId); // DB에서 찾기
 
 }
--- core/core/src/main/java/hello/core/member/MemberService.java
+++ core/core/src/main/java/hello/core/member/MemberService.java
@@ -1,7 +1,7 @@
 package hello.core.member;
 
 public interface MemberService {
-    void join(Member member);
-    Member findMember(Long memberId);
+    void join(Member member); //회원가입
+    Member findMember(Long memberId); //회원찾기
 
 }
